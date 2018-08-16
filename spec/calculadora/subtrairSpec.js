fcacd83436a4dc851d880e26d2f388762e818f22
Celio Paiva <celiopaivajr@gmail.com>
2018-08-16 14:30:07 -0300

    Add subtraction tests

---
+++ spec/calculadora/subtrairSpec.js
@@ -0,0 +1,24 @@
+describe('Sub tests', () => {
+    let Calculadora = require('../../src/js/calculadora')
+
+    it('Should return 5 for 2 and 3', () => {
+        expect(Calculadora.subtrair(8, 3)).toEqual(5)
+    });
+
+    it('Should return 6 for 9 and -3 in text format', () => {
+        expect(Calculadora.subtrair('8', '3')).toEqual(5)
+    });
+
+    it('Should return 4 for 5.5 and 1.5', () => {
+        expect(Calculadora.subtrair(5.5, 1.5)).toEqual(4)
+        expect(Calculadora.subtrair('5.5', '1.5')).toEqual(4)
+    });
+
+    it('Should validate 0 when value 1 is not a number', () => {
+        expect(Calculadora.subtrair(undefined, 5)).toEqual(0)
+    })
+
+    it('Should validate 0 when value 2 is not a number', () => {
+        expect(Calculadora.subtrair(10, undefined)).toEqual(0)
+    })
+});
--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -4,7 +4,8 @@ var Calculadora = {
         return parseFloat(num1) + parseFloat(num2);
     },
     subtrair: function (num1, num2) {
-        return 0;
+        if (isNaN(num1) || isNaN(num2)) return 0
+        return num1 - num2;
     },
     dividir: function (num1, num2) {
         return 0;
